df08ae9858f544a04f4a8dac55aac35a5c3a8ad4
sugi1410 <suganthi1410@gmail.com>
2026-04-14 23:12:33 -0500

    Refactored variables to new variables

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,11 @@ Things to reflect on:
   
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
+let customerName = "Alice";
+let numberOfItems = 5;
+let totalCost = 20;
+let message = customerName + " bought " + numberOfItems + " items for $" + totalCost + ".";
 
-console.log(d);
+console.log(message);
+
+
